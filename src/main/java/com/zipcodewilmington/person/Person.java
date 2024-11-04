b55d1bb3ab2e393bca940f67e601e062db24edd4
Sharmin Jahan <sjahan@zipcoders-mbp.mynetworksettings.com>
2024-11-03 20:22:19 -0500

    working

--- src/main/java/com/zipcodewilmington/person/Person.java
+++ src/main/java/com/zipcodewilmington/person/Person.java
@@ -3,33 +3,76 @@ package com.zipcodewilmington.person;
 /**
  * If you did ObjectsOne lab, you might recognize this code?
  */
-public class Person {
+class Person {
     private String name;
     private int age;
+    private String lastName;
+    private String placeOfBirth;
+    private String birthMonth;
+    private String nationality;
 
-    public Person() {
+//        String expectedName = "Tania";
+//        int expectedAge = 30;
+//
+//        Person person = new Person("Tania", 30);
+//        String actualName = person.getName();
+//        int actualAge = person.getAge();
+
+
+
+    public Person(String name, int age, String lastName, String placeOfBirth, String birthMonth, String nationality) {
+        this.name = name;
+        this.age = age;
+        this.lastName = lastName;
+        this.placeOfBirth = placeOfBirth;
+        this.birthMonth = birthMonth;
+        this.nationality = nationality;
     }
 
     public Person(int age) {
+        this.age = age;
     }
 
     public Person(String name) {
+        this.name = name;
     }
 
     public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
     }
 
     public void setName(String name) {
+        this.name = name;
     }
 
     public void setAge(int age) {
+        this.age = age;
     }
 
     public String getName() {
-        return null;
+        return name;
     }
 
     public Integer getAge() {
-        return null;
+        return age;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getPlaceOfBirth() {
+        return placeOfBirth;
+    }
+
+    public String getBirthMonth() {
+        return birthMonth;
+    }
+
+    public  String getNationality() {
+        return nationality;
     }
 }
+
+
